2317fb6c50737235490c58bbe6c664e3036e2d72
Lsong <song940@gmail.com>
2018-09-25 18:07:02 +0800

    smtp

--- example/index.js
+++ example/index.js
@@ -1,12 +1,14 @@
 const smtp = require('..');
 
 smtp.send({
-  from: 'mail@lsong.org',
-  to: 'test@localhost',
+  from: 'hi@lsong.org',
+  to: 'song940@gmail.com',
   subject: 'hello world',
   body: {
     _: 'This is a test message, do not reply.'
   }
 }).then(res => {
   console.log(res);
+}, err => {
+  console.error('smtp send error:', err);
 });
--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 const dns = require('dns');
-const tcp = require('net');
+const net = require('net');
+const tls = require('tls');
 const util = require('util');
+const assert = require('assert');
 const Message = require('mime2');
 const EventEmitter = require('events');
-const createReader = require('./parser');
+const createParser = require('./parser');
 
 const debug = util.debuglog('smtp2');
 
-const connect = (host, port) => {
-  return new Promise((resolve, reject) => {
-    const socket = tcp.connect(port, host, () => resolve(socket));
-    socket.once('error', reject);
-  });
-};
-
-const tryConnect = async (hosts, port = 2525) => {
-  for(const host of hosts){
-    try{
-      const socket = await connect(host, port);
-      debug(`MX connection created: ${host}:${port}`);
-      return socket;
-    }catch(e){
-      debug(`Error on connectMx for: ${host}:${port}`, e);
+const createReader = fn => {
+  const r1 = /^(\d+)-(.+)$/;
+  const r2 = /^(\d+)\s(.+)$/;
+  let results = [];
+  return createParser(line => {
+    if(r1.test(line)){
+      const m = line.match(r1);
+      results.push(m[2]);
     }
-  }
-  throw new Error('can not connect to any SMTP server');
-};
+    if(r2.test(line)){
+      const m = line.match(r2);
+      results.push(m[2]);
+      fn(m[1], results);
+      results = [];
+    }
+  });
+}
 
 class SMTP extends EventEmitter {
+  constructor(options){
+    super();
+    Object.assign(this, {
+      port: 25
+    }, options);
+  }
   resolve(domain){
     const resolveMx = util.promisify(dns.resolveMx);
     return resolveMx(domain)
@@ -39,81 +45,90 @@ class SMTP extends EventEmitter {
         .concat([domain]);
     });
   }
+  open(host, port){
+    port = port || this.port;
+    if(~host.indexOf(':')){
+      [ host, port ] = host.split(':');
+    }
+    return new Promise((resolve, reject) => {
+      const tcp = this.tls ? tls : net;
+      const socket = tcp.connect(port, host, () => resolve(socket));
+      socket.once('error', reject);
+    });
+  }
   connect(domain){
+    const tryConnect = async hosts => {
+      for(const host of hosts){
+        try{
+          const socket = await this.open(host);
+          debug(`MX connection created: ${host}`);
+          return socket;
+        }catch(e){
+          debug(`Error on connectMx for: ${host}`, e);
+        }
+      }
+      throw new Error('can not connect to any SMTP server');
+    };
     return this
       .resolve(domain)
       .then(tryConnect);
   }
-  async post(host, from, recipients, body){
-    let queue = [], step = 0;
-    queue.push(`MAIL FROM: ${from.address}`);
-    queue = queue.concat(recipients.map(address => {
-      const addr = Message.parseAddress(address);
-      return `RCPT TO: ${addr.address}`;
-    }));
-    queue.push('DATA');
-    queue.push('QUIT');
-    queue.push('');
-    const process = (sock, done, msg) => {
-      debug('<-', msg);
-      const w = b => {
-        debug('->', b);
-        sock.write(b + Message.CRLF);
+  post(host, from, recipients, body){
+    function* process(sock){
+      let res = yield;
+      assert.equal(res.code, 220, res.msg);
+      if(/ESMTP/.test(res.msg[0])){
+        res = yield `EHLO ${from.host}`;
+      }else{
+        res = yield `HELO ${from.host}`;
       }
-      const code = parseInt(msg, 10);
-      switch(code){
-        case 220:
-          if(/ESMTP/i.test(msg)){
-            w(`EHLO ${from.host}`);
-          }else{
-            w(`HELO ${from.host}`);
-          }
-          break;
-        case 250:
-          w(queue[step++]);
-          break;
-        case 354:
-          w(body);
-          w('');
-          w('.');
-          w('');
-          break;
-        case 221:
-          done(null, 'OK');
-          break;
-        default:
-          done(new Error(`SMTP responds error: ${msg}`));
-          break;
-      };
-    };
+      assert.equal(res.code, 250, res.msg);
+      res = yield `MAIL FROM:${from}`;
+      assert.equal(res.code, 250, res.msg);
+      for(const rcpt of recipients){
+        res = yield `RCPT TO:${rcpt}`;
+        assert.equal(res.code, 250, res.msg);
+      }
+      res = yield 'DATA';
+      assert.equal(res.code, 354, res.msg);
+      sock.write(`${body}\r\n\r\n`);
+      res = yield '.';
+      assert.equal(res.code, 250, res.msg);
+      res = yield 'QUIT';
+      assert.equal(res.code, 221, res.msg);
+      return res;
+    }
     return this
       .connect(host)
-      .then(socket => {
-        return new Promise((resolve, reject) => {
-          const done = (err, res) => {
-            if(err) return reject(err);
-            resolve(res);
-          };
-          socket.on('error', reject);
-          socket.on('data', createReader(
-            process.bind(socket, socket, done)
-          ));
+      .then(socket => new Promise((resolve, reject) => {
+        const gen = process(socket);
+        gen.next();
+        const reader = createReader((code, msg) => {
+          debug('->', code, msg);
+          const { done, value } = gen.next({ code, msg });
+          if(done) return resolve(value);
+          if(value){
+            debug('send:', value);
+            socket.write(`${value}\r\n`);
+          }
         });
-      });
+        socket.on('error', reject);
+        socket.on('data', reader);
+      }));
   }
   send(message){
     if(!(message instanceof Message))
       message = new Message(message);
-    let groupByHost = {}, recipients = [];
+    const recipients = [];
     if(message.to) recipients.push(message.to);
     if(message.cc) recipients.push(message.cc);
     if(message.bcc)recipients.push(message.bcc);
-    recipients = recipients.map(recipient => {
+    const groupByHost = recipients.reduce((groupByHost, recipient) => {
       const addr = Message.parseAddress(recipient);
       (groupByHost[ addr.host ] ||
-      (groupByHost[ addr.host ] = [])).push(addr.address);
-      return addr.address;
-    });
+      (groupByHost[ addr.host ] = [])).push(addr);
+      return groupByHost;
+    }, {});
     const from = Message.parseAddress(message.from);
     return Promise.all(Object.keys(groupByHost).map(domain => 
       this.post(domain, from, groupByHost[domain], message.toString())));
